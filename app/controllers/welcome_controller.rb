7f642a8b476b61bd8b03fbcf8520ff03d3f39441
Brendo Monteiro <brendomonteirodasilva@gmail.com>
2020-07-29 17:11:21 +0000

    Created logins Admin and Users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+ # Jeito simples de fazer um login e senha
+ # http_basic_authenticate_with name:"brendo", password:"123"
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
   get 'welcome/index'
 
   root to: 'welcome#index'
